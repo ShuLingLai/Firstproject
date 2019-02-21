153ca54c2b9ed8a366419d44e1f35e68b03567db
ShuLinLai <tensuon607607@gmail.com>
2019-02-21 20:10:17 +0800

    HI,HAHAHA again

---
+++ Tuple.py
@@ -0,0 +1,13 @@
+tuple1=(1, 2, 3, 4, 5)
+tuple2='1', '2', '3', '4', '5'
+
+print(tuple1[3])
+print(tuple2.index('4'))
+
+tuple = ((5, 14, 7), (23, 36, 28),(88, 80, 92))
+print(tuple[2]) #數學成績
+print(sum(tuple[0])/len(tuple[0]), sum(tuple[1])/len(tuple[1]), sum(tuple[2])/len(tuple[2]))
+
+tuple3 =((94, 90, 96),)
+print(tuple+tuple3)
+
